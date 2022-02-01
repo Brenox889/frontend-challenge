7fc6dc220269b1f6e7579be4e73f4683e1660b25
Brenox889 <brenomendes889@hotmail.com>
2022-02-01 15:15:22 -0300

    styles: CountryList styles

---
+++ src/components/CountryList/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: var(--white);
+
+`
+export const Content = styled.div`
+display: flex;
+   flex-wrap: wrap; 
+   padding: 20px 5px;
+justify-content: center;
+align-items: center;
+
+
+div {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap; 
+
+}
+
+`
