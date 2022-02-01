0b80b2a0ab4d53811ab5feef313e58edcc2995de
Brenox889 <brenomendes889@hotmail.com>
2022-02-01 15:16:26 -0300

    styles: Header styles component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  background: var(--black);
+
+  min-width: 320px;
+    margin: auto;
+    box-shadow: 0px 5px 10px 2px rgba(0,0,0,0.31); 
+`
+
+export const Content = styled.div`
+  max-width: 1440px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding: 15px 25px 25px 42px;
+
+`
